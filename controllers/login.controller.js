c56d57c5054443b4c3f3188e4e8a86f19126a5e3
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-07-31 01:41:02 +0530

    fix: changed same site to none for cookies

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -31,11 +31,12 @@ const googleLogin = async (req, res) => {
     const userData = decodeToken(token);
     userData.exp = new Date(Date.now() + 1800000);
     // console.log('login');
-
+    console.log(process.env.NODE_ENV === 'production');
     res.status(202)
       .cookie('token', token, {
         expires: new Date(Date.now() + 1800000),
         httpOnly: true,
+        sameSite: 'none',
         secure: process.env.NODE_ENV === 'production',
       })
       .send({ userData });
