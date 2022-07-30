317f4d146b893409128c30c8fdb7c123ce349377
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-07-30 21:11:35 +0530

    cors updated

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(express.urlencoded({
   extended: true,
 }));
 app.use(express.json({ limit: '50mb' }));
-app.use(cors());
+app.use(cors({
+  credentials: true,
+}));
 
 app.use('/', Router);
 
