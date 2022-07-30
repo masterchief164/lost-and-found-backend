246a8a7ac06fe8670724735c3a770c6d6f4d505e
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-07-30 19:35:44 +0530

    fix cors

--- bin/www.js
+++ bin/www.js
@@ -25,26 +25,6 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 /**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
  * Event listener for HTTP server "error" event.
  */
 
