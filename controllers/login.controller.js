a57ad33a3eb45e6d290824e77bf04dcff64d9fa4
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-07-31 01:00:25 +0530

    added catch in login

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -12,31 +12,38 @@ require('dotenv')
   .config();
 
 const googleLogin = async (req, res) => {
-  const code = req.body.tokenId;
-  console.log(code);
-  const resp = await getGoogleOAuthTokens(code);
-  // eslint-disable-next-line camelcase
-  const { id_token } = resp.data;
-  const user = jwt.decode(id_token, { complete: false });
+  try {
+    const code = req.body.tokenId;
+    console.log(code);
+    const resp = await getGoogleOAuthTokens(code);
+    console.log(resp.data);
+    // eslint-disable-next-line camelcase
+    const { id_token } = resp.data;
+    const user = jwt.decode(id_token, { complete: false });
 
-  user.access_token = resp.data.access_token;
-  user.refresh_token = resp.data.refresh_token;
-  user.roll = user.email.split('@')[0];
-  user.registeredAt = user.iat;
+    user.access_token = resp.data.access_token;
+    user.refresh_token = resp.data.refresh_token;
+    user.roll = user.email.split('@')[0];
+    user.registeredAt = user.iat;
 
-  const person = await addUser(user);
-  const token = person.hallNumber ? await createTokenProfile(person) : await createToken(person);
-  const userData = decodeToken(token);
-  userData.exp = new Date(Date.now() + 1800000);
-  // console.log('login');
+    const person = await addUser(user);
+    const token = person.hallNumber ? await createTokenProfile(person) : await createToken(person);
+    const userData = decodeToken(token);
+    userData.exp = new Date(Date.now() + 1800000);
+    // console.log('login');
 
-  res.status(202)
-    .cookie('token', token, {
-      expires: new Date(Date.now() + 1800000),
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-    })
-    .send({ userData });
+    res.status(202)
+      .cookie('token', token, {
+        expires: new Date(Date.now() + 1800000),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+      })
+      .send({ userData });
+  } catch (error) {
+    console.log(error);
+    res.status(500)
+      .send({ error });
+  }
 };
 
 const logout = (req, res) => {
