08f03791657faab2b567831cfa826260f96fdc30
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-09-07 16:30:31 +0530

    minor fixes

--- services/mail.service.js
+++ services/mail.service.js
@@ -1,11 +1,18 @@
 const transporter = require("../utils/nodemailer.config");
 
-module.exports.mailSenderService = (mailOptions) =>{
-    transporter.sendMail(mailOptions, function(err, data) {
-        if (err) {
-          console.log("Error " + err);
-        } else {
-          console.log("Email sent successfully" , data);
+module.exports.mailSenderService = async (mailOptions) => {
+    try {
+        await transporter.sendMail(mailOptions, (err, data) => {
+            if (err)
+                console.log(err);
+            else
+                console.log('email sent');
+        });
+    } catch (error) {
+        return {
+            status: 0,
+            data: error
         }
-      });
+    }
+
 }
